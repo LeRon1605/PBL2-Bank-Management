a2c2a448ff7de33f53fef47299b4bfc6aa429a6c
Ron Le <ronle9519@gmail.com>
2021-10-31 10:37:45 +0700

    Datetime v1.0

--- Src/Datetime/Datetime.h
+++ Src/Datetime/Datetime.h
@@ -1,3 +1,30 @@
+#include <iostream>
+using namespace std;
 class Date{
 	private:
+		int day, month, year;
+		int hour, minute, second;
+	public:
+		Date(const int& = -1, const int& = -1, const int& = -1, const int& = -1, const int& = -1, const int& = -1);
+		Date(const char*);
+		Date(const Date&);
+		int getDay();
+		int getMonth();
+		int getYear();
+		int getHour();
+		int getMinute();
+		int getSecond();
+		bool isLeapYear(); // Kiểm tra năm nhuận
+		bool isValidDate();
+		bool isValidHour();
+		static Date getCurrentDate();
+		void toDate(const char*); // Convert string sang Date
+
+		const Date& operator=(const Date&);
+		friend bool operator>(const Date&, const Date&);
+		friend bool operator<(const Date&, const Date&);
+		friend bool operator>=(const Date&, const Date&);
+		friend bool operator<=(const Date&, const Date&);
+		friend istream& operator>>(istream&, Date&);
+		friend ostream& operator<<(ostream&, const Date&);
 };
