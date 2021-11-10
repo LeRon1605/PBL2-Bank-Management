fd7eca79e37dc2c822864c77c950941e0c58b02a
Ron Le <ronle9519@gmail.com>
2021-11-10 10:39:57 +0700

    Finish linkedlist

--- Src/main.cpp
+++ Src/main.cpp
@@ -5,14 +5,21 @@
 #include "Repo/Repo.h"
 // #include "CTDL/Node.h"
 // #include "CTDL/LinkedList.cpp"
+bool compare(const Client A, const Client B){
+	return (Client(A).getID() > Client(B).getID());
+}
+bool test(const Client A){
+	return Client(A).getGender() == "Female";
+}
 using namespace std;
 int main(){
 	// // Code
 	Client Ron("10220013", "Ron Le", "Male", "30001234", "16/05/2002");
-	Client RonA("10220013", "Ron Le", "Female", "30001234", "16/06/2002");
-	// Card C("102200000000", Ron, "160502", 70000);
+	Client RonB("10220014", "Le Quoc Ron", "male", "30001234", "16/07/2002");
+	Client RonA("10220015", "Le Quoc Ron", "male", "30001234", "16/07/2002");
+	Card C("102200000000", Ron, "160502", 70000);
 	// Card D("102200000003", Ron, "160502", 70000);
-	// Withdraw W("10000000", C, 50000);
+	Withdraw W("10000000", C, 50000);
 	// Transaction *ptr = new Withdraw(W);
 	// ptr -> makeTransaction(C.getPin());
 	// ptr -> show();
@@ -36,6 +43,22 @@ int main(){
 	// ofstream a;
 	// a.open("../Data/M.txt");
 	// a << 3; 
-	Repository<Client>::findAndRemove("10220013", "Client.txt");
+	// cout << Repository<Client>::findAndUpdate(RonA, "Client.txt");
+	// cout << Repository<Client>::findAndRemove("10220013", "Client.txt");
+	//Client A = Repository<Client>::getByID("10220014", "Client.txt"); // copy constructor <-
+	// A.show();
+	// LinkedList<Transaction*> list;
+	// Transaction *ptr = new Withdraw("10000000", C, 50000);
+	// list.add(ptr);
+	// list.show();
+
+	LinkedList<Client> listB;
+	listB.addTail(Ron);
+	listB.addTail(RonB);
+	listB.addTail(RonA);
+	listB.insertAt(RonA, 1);
+	//listB.sort(compare);
+	cout << listB.remove(test);
+	listB.show();
 	return 0;
 }
