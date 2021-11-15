3d0064446c11b78bb8e516c813da374375173665
= <=>
2021-11-15 23:53:38 +0700

    Menu

---
+++ Src/Menu/menu.h
@@ -0,0 +1,17 @@
+class menu
+{
+private:
+    int lc;
+public:
+    menu();
+    ~menu();
+    void background();
+    void showMenu();
+    void showMenuClientManager();
+    void showMenuCardManager();
+    void showMenuTransactionManager();
+    void selection();
+};
+
+
+
